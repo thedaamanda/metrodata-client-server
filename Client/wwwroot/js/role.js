8573cb2a3a219b99e4e33976620f85fe838c5f0a
thedaamanda <thedaamandaa@gmail.com>
2023-05-04 10:26:43 +0700

    refactor: change table to datatable

--- Client/wwwroot/js/role.js
+++ Client/wwwroot/js/role.js
@@ -1,33 +1,55 @@
-const roleListElement = document.querySelector("#role-list");
-
 const getRoles = async () => {
     try {
         const result = await DataSource.getRoles();
         renderResult(result);
     } catch (message) {
-        fallbackResult(message)
+        fallbackResult(message);
     }
-}
-
-const renderResult = results => {
-    let data = '';
-    let no = 1;
-    results.forEach(result => {
-        data += `
-        <tr>
-            <td>${no++}</td>
-            <td>${result.name}</td>
-            <td>
-                <a href="#" class="btn btn-outline-warning">Edit</a>
-                <a href="#" class="btn btn-outline-danger">Hapus</a>
-            </td>
-        </tr>
-        `;
-    });
-    roleListElement.innerHTML = data;
 };
 
-const fallbackResult = message => {
+const renderResult = (results) => {
+    $("#role-table").dataTable({
+        dom: "Bfrtip",
+        buttons: [
+            { extend: 'copy', text: 'Copy', titleAttr: 'Copy' },
+            { extend: 'csv', text: 'CSV', titleAttr: 'CSV' },
+            { extend: 'excel', text: 'Excel', titleAttr: 'Excel' },
+            { extend: 'pdf', text: 'PDF', titleAttr: 'PDF' },
+            { extend: 'print', text: 'Print', titleAttr: 'Print' }
+        ],
+        data: results,
+        columns: [
+            {
+                data: null,
+                render: function (data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                }
+            },
+            { data: "name" },
+            {
+                data: "",
+                render: function (data, type, row) {
+                    return `
+                        <button class="btn btn-icon icon-left btn-warning dt-edit" onclick="edit()" data-bs-toggle="modal" data-bs-target="#modalEdit">
+                            <a data-bs-toggle="tooltip" data-bs-placement="top" title="Edit">
+                                <i class="far fa-edit"></i> Edit
+                            </a>
+                        </button>
+
+                        <button class="btn btn-danger dt-delete" id="hapus" onclick="remove(${row['nik']})" data-bs-toggle="modal" data-bs-target="#modalDelete">
+                            <a data-bs-toggle="tooltip" data-bs-placement="top" title="Delete">
+                                <i class="far fa-trash-alt"></i> Delete
+                            </a>
+                        </button>
+					`;
+                }
+            },
+        ],
+    });
+};
+
+const fallbackResult = (message) => {
+    const roleListElement = document.querySelector("#role-list");
     roleListElement.innerHTML = `
         <tr>
             <td colspan="3">${message}</td>
@@ -35,4 +57,8 @@ const fallbackResult = message => {
     `;
 };
 
-getRoles();
+const main = () => {
+    getRoles();
+};
+
+main();
--- Client/wwwroot/js/university.js
+++ Client/wwwroot/js/university.js
@@ -1,5 +1,3 @@
-const universityListElement = document.querySelector("#university-list");
-
 const getUniversities = async () => {
     try {
         const result = await DataSource.getUniversities();
@@ -10,24 +8,48 @@ const getUniversities = async () => {
 };
 
 const renderResult = (results) => {
-    let data = "";
-    let no = 1;
-    results.forEach((result) => {
-        data += `
-        <tr>
-            <td>${no++}</td>
-            <td>${result.name}</td>
-            <td>
-                <a href="#" class="btn btn-outline-warning">Edit</a>
-                <a href="#" class="btn btn-outline-danger">Hapus</a>
-            </td>
-        </tr>
-        `;
+    $("#university-table").dataTable({
+        dom: "Bfrtip",
+        buttons: [
+            { extend: 'copy', text: 'Copy', titleAttr: 'Copy' },
+            { extend: 'csv', text: 'CSV', titleAttr: 'CSV' },
+            { extend: 'excel', text: 'Excel', titleAttr: 'Excel' },
+            { extend: 'pdf', text: 'PDF', titleAttr: 'PDF' },
+            { extend: 'print', text: 'Print', titleAttr: 'Print' }
+        ],
+        data: results,
+        columns: [
+            {
+                data: null,
+                render: function (data, type, row, meta) {
+                    return meta.row + meta.settings._iDisplayStart + 1;
+                }
+            },
+            { data: "name" },
+            {
+                data: "",
+                render: function (data, type, row) {
+                    return `
+                        <button class="btn btn-icon icon-left btn-warning dt-edit" onclick="edit()" data-bs-toggle="modal" data-bs-target="#modalEdit">
+                            <a data-bs-toggle="tooltip" data-bs-placement="top" title="Edit">
+                                <i class="far fa-edit"></i> Edit
+                            </a>
+                        </button>
+
+                        <button class="btn btn-danger dt-delete" id="hapus" onclick="remove(${row['nik']})" data-bs-toggle="modal" data-bs-target="#modalDelete">
+                            <a data-bs-toggle="tooltip" data-bs-placement="top" title="Delete">
+                                <i class="far fa-trash-alt"></i> Delete
+                            </a>
+                        </button>
+					`;
+                }
+            },
+        ],
     });
-    universityListElement.innerHTML = data;
 };
 
 const fallbackResult = (message) => {
+    const universityListElement = document.querySelector("#university-list");
     universityListElement.innerHTML = `
         <tr>
             <td colspan="3">${message}</td>
@@ -37,10 +59,6 @@ const fallbackResult = (message) => {
 
 const main = () => {
     getUniversities();
-
-    // $("#table-university").dataTable({
-    //     columnDefs: [{ sortable: false, targets: [2] }],
-    // });
 };
 
 main();
